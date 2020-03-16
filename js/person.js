090a545bba75a646b38cacd5a346b8eee1209e0c
Paulo Henrique Martins <paulovelho@paulovelho.com>
2020-03-16 15:05:02 -0300

    new moving version

--- js/person.js
+++ js/person.js
@@ -1,26 +1,145 @@
-var person = function(){
+// Person object
+function Person(svg, x, y, id, aoa) {
+	this.id = id; // id of ball
 
-	this.blockHeight = 20;
+	this.posX = x; // cx
+	this.posY = y; // cy
+	this.svg = svg; // parent SVG
+	this.aoa = aoa; // initial angle of attack
+	this.data = [this.id]; // allow us to use d3.enter()
 
-	this.id = 0;
-	this.status = "n"; 
-	// n = "normal" | i = "infected" | "d" = "dead"
+	this.weight = 5;
+	this.radius = this.weight;
 
+	this.status = "n";
+	// n = "normal" | i = "infected" | d = "dead" | c = "cure"
+	this.ticksSinceInfection = 0;
+	this.moving = true;
 
+	// **** aoa is used only here -- earlier I was using to next move position.
+	// Now aoa and speed together is velocity 
+	this.vx = Math.cos(this.aoa); // velocity x
+	this.vy = Math.sin(this.aoa); // velocity y
+	this.initialVx = this.vx;
+	this.initialVy = this.vy;
+	this.initialPosX = this.posX;
+	this.initialPosY = this.posY;
+
+	this.getColor = () => {
+		switch(this.status) {
+			case "i":
+				return "red";
+			case "c":
+				return "green";
+			case "d":
+				return "#000";
+			case "n":
+			default:
+				return '#66ccff';
+		}
+	}
 
 	this.infect = () => {
-		console.info("infecting....");
 		this.status = "i";
+		this.getBall().style("fill", this.getColor());
+	}
+	this.cure = () => {
+		this.status = "c";
+		this.getBall().style("fill", this.getColor());
+	}
+	this.quarantine = () => {
+		this.moving = false;
+		this.vx = 0;
+		this.vy = 0;
+	}
+	this.die = () => {
+		this.status = "d";
+		this.moving = false;
+		this.getBall().style("fill", this.getColor());
+	}
+
+
+	// wip
+	this.diseaseProgression = () => {
+		this.ticksSinceInfection ++;
+		if(this.ticksSinceInfection < 500) return false;
+		if(this.ticksSinceInfection % 10 != 0) return false; // when to check?
+		if(this.ticksSinceInfection > 1200) {
+			console.info("killing", this);
+			this.die();
+			return false;
+		}
+		let chance = (this.ticksSinceInfection/100 - 45) / 100;
+		if(this.probability(chance)) {
+			this.cure();
+		}
+
 	}
 
 
 
+	this.probability = (n) => {
+		return Math.random() < n;
+	}
 
 
-};
+	// technical functions:
+	this.getBall = () => {
+		return this.svg.selectAll('#' + this.id).data(this.data);
+	}
 
-person.prototype.toString = function(){
-	return "[person "+this.id+" ="+this.status+"|]";
-};
+	this.Draw = () => {
+		var ball = this.getBall();
+		ball.enter()
+			.append("circle")
+			.attr({"id" : this.id, 'class' : 'ball', 'r' : this.radius, 'weight' : this.weight})
+			.style("fill", this.getColor())
+			.on("click", () => {
+				console.info("clicked on ", this);
+			});
+		ball.attr("cx", this.posX).attr("cy", this.posY);
+	}
+
+	this.Move = function () {
+//		this.diseaseProgression();
+		if(!this.moving) {
+			return true;
+		}
+		this.posX += this.vx;
+		this.posY += this.vy;
+
+		if (parseInt(this.svg.attr('width')) <= (this.posX + this.radius)) {
+			this.posX = parseInt(svg.attr('width')) - this.radius - 1;
+			this.aoa = Math.PI - this.aoa;
+			this.vx = -this.vx;
+		}
+
+		if ( this.posX < this.radius) {
+			this.posX = this.radius+1;
+			this.aoa = Math.PI - this.aoa;
+			this.vx = -this.vx;
+		}
+
+		if (parseInt(this.svg.attr('height')) < (this.posY + this.radius)) {
+			this.posY = parseInt(svg.attr('height')) - this.radius - 1;
+			this.aoa = 2 * Math.PI - this.aoa;
+			this.vy = -this.vy;
+		}
+
+		if (this.posY < this.radius) {
+			this.posY = this.radius+1;
+			this.aoa = 2 * Math.PI - this.aoa;
+			this.vy = -this.vy;
+		}
+
+		// **** NOT USING AOA except during initilization. Just left this for future reference ***** 
+		if (this.aoa > 2 * Math.PI)
+			this.aoa = this.aoa - 2 * Math.PI;
+		if (this.aoa < 0)
+			this.aoa = 2 * Math.PI + this.aoa;
+
+		this.Draw();
+	}
+}
 
 
--- js/who.js
+++ js/who.js
@@ -1,140 +1,225 @@
-angular
-.module("nrt-pandemic")
-.controller("whoController",
-function($scope, $rootScope, $interval, $sce){
-	
-	// variables:
-	$scope.size = 62;
-	$scope.censo = 62 * 62;
+// based on http://bl.ocks.org/atul-github/0019158da5d2f8499f7f
 
-	$scope.map = [];
+var who = function() {
 
-	$scope.moveTranslation = 12;
-	$scope.moves = {
-		colodd: 0,
-		coleven: 0,
-		rowodd: 0,
-		roweven: 0
-	};
+	this.svg = null;
+	this.population = []; // global array representing balls
 
-	// interaction events
-//	$scope.$on("play-pause", function(){ $scope.playpause(); });
+	this.infected = 1;
 
 
+	this.CheckCollision = (ball1, ball2) => {
+		var absx = Math.abs(parseFloat(ball2.posX) - parseFloat(ball1.posX));
+		var absy = Math.abs(parseFloat(ball2.posY) - parseFloat(ball1.posY));
 
-	// setup:
-	$scope.populate = () => {
-		createNewMap();
-		for (var i = 5; i > 0; i--) {
-			infectRandom();
+		// find distance between two balls.
+		var distance = (absx * absx) + (absy * absy);
+		distance = Math.sqrt(distance);
+		// check if distance is less than sum of two radius - if yes, collision
+		if (distance < (parseFloat(ball1.radius) + parseFloat(ball2.radius))) {
+			return true;
+		}
+		return false;
+	}
+
+
+	this.transmit = (sick, healthy) => {
+		healthy.infect();
+		this.infected++;
+	}
+
+	this.processStatus = (p1, p2) => {
+		if( p1.status == p2.status ) return true;
+		if( p1.status == "i" && p2.status == "n" ){
+			this.transmit(p1, p2);
+		}
+		if( p2.status == "i" && p1.status == "n" ){
+			this.transmit(p2, p1);
+		}
+		document.getElementById("infectCount").innerHTML = this.infected;
+	}
+
+	this.StaticCollision = (static, moving) => {
+		if(moving.vy > moving.vx){
+			moving.aoa = 2 * Math.PI - moving.aoa;
+		} else {
+			moving.aoa = Math.PI - moving.aoa;
+		}
+//		moving.aoa = Math.PI - moving.aoa;
+		moving.vx = -moving.vx;
+		moving.vy = -moving.vy;
+	}
+	this.MovingCollision = (ball1, ball2) => {
+		// calculate new velocity of each ball.
+		var vx1 = (2 * ball2.weight * ball2.vx) / (ball1.weight + ball2.weight);
+		var vy1 = (2 * ball2.weight * ball2.vy) / (ball1.weight + ball2.weight);
+		var vx2 = (2 * ball1.weight * ball1.vx) / (ball1.weight + ball2.weight);
+		var vy2 = (2 * ball1.weight * ball1.vy) / (ball1.weight + ball2.weight);
+
+		//set velocities for both balls
+		ball1.vx = vx1;
+		ball1.vy = vy1;
+		ball2.vx = vx2;
+		ball2.vy = vy2;
+	}
+
+	// courtsey thanks to several internet sites for formulas
+	// detect collision, find intersecting point and set new speed+direction for each ball based on weight (weight=radius)
+	this.ProcessCollision = (ball1, ball2) => {
+
+		if (ball2 <= ball1)
+			return;
+		if (ball1 >= (this.population.length-1) || ball2 >= this.population.length )
+			return;
+
+		ball1 = this.population[ball1];
+		ball2 = this.population[ball2];
+
+		if ( this.CheckCollision(ball1, ball2) ) {
+			if(!ball1.moving) {
+				this.StaticCollision(ball1, ball2);
+			} else if (!ball2.moving) {
+				this.StaticCollision(ball2, ball1);
+			} else {
+				this.MovingCollision(ball1, ball2);
+			}
+
+			//ensure one ball is not inside others. distant apart till not colliding
+			while (this.CheckCollision(ball1, ball2)) {
+				ball1.posX += ball1.vx;
+				ball1.posY += ball1.vy;
+
+				ball2.posX += ball2.vx;
+				ball2.posY += ball2.vy;
+			}
+
+			this.processStatus(ball1, ball2);
+
+			ball1.Draw();
+			ball2.Draw();
 		}
 	}
-	var createNewMap = () => {
-		for (var i = 0; i < $scope.size; i++) {
-			$scope.map[i] = [];
-			for (var j = 0; j < $scope.size; j++) {
-				let p = new person();
-				$scope.map[i][j] = p;
+
+	this.startStopFlag = null;
+	this.tick = () => {
+		for (var i = 0; i < this.population.length; ++i) {
+			this.population[i].Move();
+			for (var j = i + 1; j < this.population.length; ++j) {
+				this.ProcessCollision(i, j);
 			}
 		}
-		$scope.map[61][42].infect();
-	}
-	var infectRandom = () => {
-		let random = selectRandom();
-		$scope.map[random.i][random.j].infect();
+		if (this.startStopFlag == null)
+			return true;
+		else
+			return false;
 	}
 
-	// tick
-	var resetMove = () => {
-		$scope.moves = {
-			colodd: 0,
-			coleven: 0,
-			rowodd: 0,
-			roweven: 0
-		};
-
+	this.StartStopGame = () => {
+		if (this.startStopFlag == null) {
+			d3.timer(this.tick, 500);
+			this.startStopFlag = 1;
+		} else {
+			this.startStopFlag = null;
+		}
 	}
 
 
 
 
+	// helpers: 
+	this.getRandom = (max) => {
+		return Math.floor(Math.random() * Math.floor(max));
+	}
+	this.probability = (n) => {
+		return Math.random() < n;
+	}
 
 
-	// move:
-	$scope.animate = null;
-	var moveAnimate = (moveTo, stepFn) => {
-		$($scope.moves).animate(moveTo, {
-			duration: 1000,
-			step: stepFn,
-			done: () => {
-				console.info($scope.map);
+	// setup
+	this.infectRandom = () => {
+		let rand = this.getRandom(199);
+		this.population[rand-1].infect();
+	}
+
+	this.populate = () => {
+		const rowOfBalls = (row) => {
+			let minh = (row-1)*59 + 5;
+			if(row == 10) minh -= 5;
+
+			let pid = this.population.length - 1;
+
+			for(let i=1; i <= 20; i++) {
+				let h = minh + this.getRandom(60);
+				let w = (i-1) * 40 + this.getRandom(25);
+				let p = new Person(this.svg, w+5, h, 'n' + (i + pid), this.getRandom(360));
+				if(this.probability(0.50)) p.quarantine();
+				this.population.push(p);
 			}
-		});
+		}
+
+		for (var i = 1; i <= 10; i++) {
+			rowOfBalls(i);
+		}
+
+		for (var i = 0; i < this.population.length; ++i) {
+			for (var j = i + 1; j < this.population.length; ++j) {
+				this.ProcessCollision(i, j);
+			}
+		}
+
+		this.infectRandom();
+
+		for (var i = 0; i < this.population.length; ++i) {
+			this.population[i].Draw();
+		}
 	}
-	var moveAnimateCol = function() {
-		console.info("up to ", this);
-		$(".col-even").css("transform", `translateY(${this.coleven}px)`); 
-		$(".col-odd").css("transform", `translateY(${this.colodd}px)`);
+	this.createCanvas = () => {
+		const containerId = "drawArea";
+		var height = document.getElementById(containerId).clientHeight;
+		var width = document.getElementById(containerId).clientWidth;
+		gContainerId = containerId;
+		gCanvasId = containerId + '_canvas';
+		gTopGroupId = containerId + '_topGroup';
+		return d3.select("#" + containerId).append("svg")
+			.attr("id", gCanvasId)
+			.attr("width", width)
+			.attr("height", height)
+			.append("g")
+			.attr("id", gTopGroupId)
+			.attr("x", 0)
+			.attr("y", 0)
+			.attr("width", width)
+			.attr("height", height)
+			.style("fill", "none");
 	}
-	var moveAnimateRow = function() {
-		console.info("side to ", this);
-		$(".row-even").css("transform", `translateX(${this.roweven}px)`); 
-		$(".row-odd").css("transform", `translateX(${this.rowodd}px)`); 
-	}
-
-	$scope.movedown = () => {
-		let end = JSON.parse(JSON.stringify($scope.moves));
-		end.coleven -= $scope.moveTranslation;
-		end.colodd += $scope.moveTranslation;
-		moveAnimate(end, moveAnimateCol);
-	}
-	$scope.moveup = () => {
-		let end = JSON.parse(JSON.stringify($scope.moves));
-		end.coleven += $scope.moveTranslation;
-		end.colodd -= $scope.moveTranslation;
-		moveAnimate(end, moveAnimateCol);
-	}
-	$scope.moveleft = () => {
-		let end = JSON.parse(JSON.stringify($scope.moves));
-		end.roweven -= $scope.moveTranslation;
-		end.rowodd += $scope.moveTranslation;
-		moveAnimate(end, moveAnimateRow);
-	}
-	$scope.moveright = () => {
-		let end = JSON.parse(JSON.stringify($scope.moves));
-		end.roweven += $scope.moveTranslation;
-		end.roeodd -= $scope.moveTranslation;
-		moveAnimate(end, moveAnimateRow);
-	}
-
-
-
-
-
-	// utils
-	var selectRandom = () => {
-		return {
-			i: Math.floor(Math.random() * Math.floor($scope.size)),
-			j: Math.floor(Math.random() * Math.floor($scope.size))
-		};
-	}
-
-
-
-
-
-
-
-	$scope.play = () => {
-		$scope.movedown();
+	this.keyboard = () => {
+		// I always like to handle ESC key
+		d3.select('body')
+				.on('keydown', () =>  {
+					if (this.population.length == 0)
+						return;
+					console.log(d3.event);
+					switch(d3.event.keyCode) {
+						case 27: // ESC
+							this.StartStopGame();
+						case 32: // SPACE
+							this.StartStopGame();
+						case 16: // SHIFT
+							this.tick();
+					}
+				});
 	}
 
-	var initialize = () => {
-		console.info("initialize");
-		$scope.populate();
-	};
-	initialize();
+	this.Initialize = () => {
+		this.svg = this.createCanvas();
 
-});
+		this.populate();
+		this.keyboard();
+		return this.svg;
+	}
+
+
+
+}
 
 
